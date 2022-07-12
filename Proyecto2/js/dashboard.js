8ffbe3efdc41383d64e53899561a4f40257ea232
Jairo <jairoalc@espol.edu.ec>
2022-07-11 21:18:20 -0500

    final version

--- Proyecto2/js/dashboard.js
+++ Proyecto2/js/dashboard.js
@@ -1,16 +1,12 @@
-/*
-Template Name: Admin Pro Admin
-Author: Wrappixel
-Email: niravjoshi87@gmail.com
-File: js
-*/
-
 const today = new Date();
+
+const day = today.getDay()
 const dd = String(today.getDate()).padStart(2, '0');
 const mm = String(today.getMonth() + 1).padStart(2, '0');
 const yyyy = today.getFullYear();
 
 const hours = [00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+const weekDays = ["Lun", "Mar", "Mie", "Jue", "Vie", "Sab", "Dom"]
 
 fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin')
   .then(r => r.json())
@@ -90,9 +86,18 @@ const cargandoBarras = (hours = [], temperature = []) => {
 }
 
 const cargandoMaxMin = (max = [], min = []) => {
+  const actualWeek = weekDays.slice(day - 1)
+  console.log(actualWeek)
+  const restWeek = actualWeek.length < 7 ? weekDays.slice(0, actualWeek.length-1) : undefined
+  console.log(restWeek)
+  const week = restWeek ? [...actualWeek, ...restWeek] : [...actualWeek]
+
+
+  console.log(week)
+
   new Chartist.Line('#sales-overview3',
     {
-      labels: [parseInt(dd), parseInt(dd) + 1, parseInt(dd) + 2, parseInt(dd) + 3, parseInt(dd) + 4, parseInt(dd) + 5, parseInt(dd) + 6],
+      labels: week,
       series: [
         { meta: "Min Temperature (°C)", data: min },
         { meta: "Max Temperature (°C)", data: max }
@@ -188,61 +193,3 @@ const cargandoPie = (prom_list = []) => {
   });
 
 }
-
-
-// ============================================================== 
-// Website Visitor
-// ============================================================== 
-
-var chart = new Chartist.Line('.website-visitor', {
-  labels: [1, 2, 3, 4, 5, 6, 7, 8],
-  series: [
-    [0, 5, 6, 8, 25, 9, 8, 24]
-    , [0, 3, 1, 2, 8, 1, 5, 1]
-  ]
-}, {
-  low: 0,
-  high: 28,
-  showArea: true,
-  fullWidth: true,
-  plugins: [
-    Chartist.plugins.tooltip()
-  ],
-  axisY: {
-    onlyInteger: true
-    , scaleMinSpace: 40
-    , offset: 20
-    , labelInterpolationFnc: function (value) {
-      return (value / 1) + 'k';
-    }
-  },
-});
-
-// Offset x1 a tiny amount so that the straight stroke gets a bounding box
-// Straight lines don't get a bounding box 
-// Last remark on -> http://www.w3.org/TR/SVG11/coords.html#ObjectBoundingBox
-chart.on('draw', function (ctx) {
-  if (ctx.type === 'area') {
-    ctx.element.attr({
-      x1: ctx.x1 + 0.001
-    });
-  }
-});
-
-// Create the gradient definition on created event (always after chart re-render)
-chart.on('created', function (ctx) {
-  var defs = ctx.svg.elem('defs');
-  defs.elem('linearGradient', {
-    id: 'gradient',
-    x1: 0,
-    y1: 1,
-    x2: 0,
-    y2: 0
-  }).elem('stop', {
-    offset: 0,
-    'stop-color': 'rgba(255, 255, 255, 1)'
-  }).parent().elem('stop', {
-    offset: 1,
-    'stop-color': 'rgba(38, 198, 218, 1)'
-  });
-});
